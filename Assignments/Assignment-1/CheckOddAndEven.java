b0e4c5b240cc3f61678ab7634647a253e126ad8d
sabakazicmaug26-creator <saba.kazi.cmaug26@gmail.com>
2026-08-19 20:58:15 +0530

    Add files via upload

---
+++ Assignments/Assignment-1/CheckOddAndEven.java
@@ -0,0 +1,9 @@
+class OddAndEven{
+public static void main(String args[]){
+int n = 17;
+if (n % 2 == 0)
+System.out.println("Even");
+else
+System.out.println("Odd");
+}
+}
---
+++ Assignments/Assignment-1/CheckPositiveNumber.java
@@ -0,0 +1,10 @@
+class PositiveNumber{
+        public static void main(String args[]){
+        int n = 10;
+        if (n>0)
+        System.out.println("Positive");
+        else
+        System.out.println("Not Positive");
+
+}
+}
