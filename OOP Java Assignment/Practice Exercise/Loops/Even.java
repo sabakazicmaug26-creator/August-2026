e6981aa260cdb6c109fcd1c4b5010f960f223656
sabakazicmaug26-creator <saba.kazi.cmaug26@gmail.com>
2026-09-15 11:34:22 +0530

    Add files via upload

---
+++ OOP Java Assignment/Practice Exercise/Loops/Even.java
@@ -0,0 +1,13 @@
+
+public class Even {
+
+	public static void main(String[] args) {
+		for(int i=1; i<500; i++) {
+			if(i % 2 == 0) {
+				System.out.println("Even number: " + i);
+			}
+		}
+
+	}
+
+}
---
+++ OOP Java Assignment/Practice Exercise/Loops/Odd.java
@@ -0,0 +1,14 @@
+
+public class Odd {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<1000; i++) {
+			if(i % 2 != 0) {
+				System.out.println("Odd Numbers: " + i);
+			}
+		}
+
+	}
+
+}
